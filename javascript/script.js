87d120b1fb519fd3d770bde329710d7c50f90eff
junyoung144 <khm132@naver.com>
2024-05-15 01:54:58 +0900

    인덱스 배너 추가 및 폰트 추가

--- javascript/script.js
+++ javascript/script.js
@@ -4,3 +4,39 @@ $(function() {
     $("#footer").load("footer.html");
 });
 
+
+
+$('.banner_box_list').slick({
+    speed: 1200,
+    dots: false,
+    arrows:false,
+    autoplay: true,
+    slidesToShow: 1,
+    autoplaySpeed: 10000,
+    slidesToScroll: 1,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+        }
+    },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+        }
+    },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
